475e940dc2a4882c3cd80e12caa9d314d5e50252
ghxm <max.haag@gmx.net>
2021-06-08 09:20:02 +0200

    make final event doc creation optional

--- R/euplexdata.R
+++ R/euplexdata.R
@@ -1,13 +1,13 @@
 # Main euplexdata function
 
 #' @export
-euplexdata <- function (df, remove_extra_vars=TRUE, rename_vars = TRUE, request_eurovoc = FALSE, rm_raw = TRUE){
+euplexdata <- function (df, remove_extra_vars=TRUE, rename_vars = TRUE, request_eurovoc = FALSE, rm_raw = TRUE, docs_events=c('proposal', 'final')){
 
     df <- df %>%
         reformat_missing_data() %>%
         reformat_date_variables() %>%
         reformat_logical_variables() %>%
-        create_named_procedure_event_variables(event_name = "final", event_codes = c("_PUB_OJ_", "_SIGN_byEP_CONSIL_", "_ADP_FRM_byCONSIL_")) %>%
+        {if ('final' %in% docs_events) create_named_procedure_event_variables(event_name = "final", event_codes = c("_PUB_OJ_", "_SIGN_byEP_CONSIL_", "_ADP_FRM_byCONSIL_")) else .}%>%
         create_named_procedure_event_variables(event_name = "proposal", event_codes = c("_ADP_byCOM_")) %>%
         apply_correction_data() %>%
         {if(remove_extra_vars) remove_extra_variables(.) else .} %>%
