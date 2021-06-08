85504b45afdb66b29d08da32db804a8ff2419b01
ghxm <max.haag@gmx.net>
2021-06-08 09:33:44 +0200

    spelling errors again

--- R/euplexdata.R
+++ R/euplexdata.R
@@ -7,7 +7,7 @@ euplexdata <- function (df, remove_extra_vars=TRUE, rename_vars = TRUE, request_
         reformat_missing_data() %>%
         reformat_date_variables() %>%
         reformat_logical_variables() %>%
-        {if ('final' %in% docs_events) create_named_procedure_event_variables(event_name = "final", event_codes = c("_PUB_OJ_", "_SIGN_byEP_CONSIL_", "_ADP_FRM_byCONSIL_")) else .}%>%
+        {if ('final' %in% events_docs) create_named_procedure_event_variables(event_name = "final", event_codes = c("_PUB_OJ_", "_SIGN_byEP_CONSIL_", "_ADP_FRM_byCONSIL_")) else .}%>%
         create_named_procedure_event_variables(event_name = "proposal", event_codes = c("_ADP_byCOM_")) %>%
         apply_correction_data() %>%
         {if(remove_extra_vars) remove_extra_variables(.) else .} %>%
